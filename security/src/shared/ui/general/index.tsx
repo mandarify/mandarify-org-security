71175928fa4c62fe484cff1f1b1bf6b869fd5382
MANDARIFY <mandarify@gmail.com>
2025-02-12 22:29:18 +0100

    issue 2

--- security/src/shared/ui/general/index.tsx
+++ security/src/shared/ui/general/index.tsx
@@ -1,9 +1,5 @@
 import SvgIcon from "./SvgIcon/SvgIcon";
-import Button from "./Button/Button";
-import ButtonLink from "./ButtonLink/ButtonLink";
 
 export {
    SvgIcon,
-   Button,
-   ButtonLink,
 };
